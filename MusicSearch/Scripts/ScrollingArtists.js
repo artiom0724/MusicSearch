c401a69ae64e16f8872de05b625fa88a40a96279
artiom0724 <artiom0724@gmail.com>
2017-09-07 19:34:30 +0300

    styles in Content/Style, scripts in Scripts/Scrolling**

---
+++ MusicSearch/Scripts/ScrollingArtists.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+
+        $('div#loading').hide();
+
+        var page = 1;
+        var _inCallback = false;
+        function loadItems() {
+                if (page > 0 && !_inCallback) {
+                        _inCallback = true;
+                        page++;
+                        $('img#loading').show();
+
+                        $.ajax({
+                                type: 'GET',
+                                url: '/Home/Index/' + page.toString(),
+                                success: function (data, textstatus) {
+                                        if (data != '') {
+                                                $("#scrolList").append(data);
+                                        }
+                                        else {
+                                                page = 1;
+                                        }
+                                        _inCallback = false;
+                                        $("img#loading").hide();
+                                }
+                        });
+                }
+        }
+
+        $(window).scroll(function () {
+                if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+                        loadItems();
+                }
+        });
+})
